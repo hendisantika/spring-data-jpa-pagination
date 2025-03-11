d6f496a3a420ab34e0940f566f6fa71003197964
hendisantika <hendisantika@yahoo.co.id>
2025-03-12 06:03:32 +0700

    feat: Add UserRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jpapagination/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.jpapagination.repository;
+
+import id.my.hendisantika.jpapagination.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-jpa-pagination
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/03/25
+ * Time: 06.03
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    Page<User> findByNameContaining(String name, Pageable pageable);
+}
