57df7ef64cfb34f672be8100fb2c58d06318484a
hendisantika <hendisantika@yahoo.co.id>
2025-03-12 06:07:50 +0700

    feat: Add getUsers in UserController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/jpapagination/controller/UserController.java
+++ src/main/java/id/my/hendisantika/jpapagination/controller/UserController.java
@@ -1,9 +1,20 @@
 package id.my.hendisantika.jpapagination.controller;
 
+import id.my.hendisantika.jpapagination.entity.HttpResponse;
 import id.my.hendisantika.jpapagination.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import static org.springframework.http.HttpStatus.OK;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,9 +28,33 @@ import org.springframework.web.bind.annotation.RestController;
  * To change this template use File | Settings | File Templates.
  */
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<HttpResponse> getUsers(@RequestParam Optional<String> name,
+                                                 @RequestParam Optional<Integer> page,
+                                                 @RequestParam Optional<Integer> size) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
+        //throw new RuntimeException("Forced exception for testing");
+        return ResponseEntity.ok().body(
+                HttpResponse.builder()
+                        .timeStamp(LocalTime.now().toString())
+                        .data(
+                                Map.of("page",
+                                        userService.getUsers(
+                                                name.orElse(""),
+                                                page.orElse(0), /* if page is not specified, default to 0 */
+                                                size.orElse(10) /* if size is not specified, default to 10 */
+                                        )
+                                ))
+                        .message("Users Retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build());
+
+    }
 }
