459cc05841f9f8e9d5b938ba578c099b990967a1
hendisantika <hendisantika@yahoo.co.id>
2025-03-12 06:16:14 +0700

    fix: codes & mysql compose.yaml🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/jpapagination/repository/UserRepository.java
+++ src/main/java/id/my/hendisantika/jpapagination/repository/UserRepository.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.jpapagination.repository;
 import id.my.hendisantika.jpapagination.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
@@ -16,6 +17,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * Time: 06.03
  * To change this template use File | Settings | File Templates.
  */
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, PagingAndSortingRepository<User, Long> {
     Page<User> findByNameContaining(String name, Pageable pageable);
 }
