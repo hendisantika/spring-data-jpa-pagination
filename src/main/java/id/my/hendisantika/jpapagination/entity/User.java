a718382da63fe8859e2e96e01502a2859fff5add
hendisantika <hendisantika@yahoo.co.id>
2025-03-12 06:02:05 +0700

    feat: Add User Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jpapagination/entity/User.java
@@ -0,0 +1,42 @@
+package id.my.hendisantika.jpapagination.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-jpa-pagination
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/03/25
+ * Time: 06.01
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT) /* to exclude null values */
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String status;
+    private String address;
+    private String phone;
+    private String imageUrl;
+}
