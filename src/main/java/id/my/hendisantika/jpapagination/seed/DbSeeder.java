4a1db13ea05afe472ccf386ebbdadfdc6ade573c
hendisantika <hendisantika@yahoo.co.id>
2025-03-12 06:05:17 +0700

    feat: Add DbSeeder🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jpapagination/seed/DbSeeder.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.jpapagination.seed;
+
+import com.github.javafaker.Faker;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-jpa-pagination
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/03/25
+ * Time: 06.04
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class DbSeeder {
+
+    private static final Faker faker = new Faker();
+}
