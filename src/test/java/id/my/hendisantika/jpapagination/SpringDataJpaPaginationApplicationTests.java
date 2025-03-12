beef338e5000eb4c09e8d8177c27adfb9ff74a9a
hendisantika <hendisantika@yahoo.co.id>
2025-03-12 07:40:01 +0700

    feat: Add testSaveUser Test🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/jpapagination/SpringDataJpaPaginationApplicationTests.java
+++ src/test/java/id/my/hendisantika/jpapagination/SpringDataJpaPaginationApplicationTests.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.jpapagination;
 
+import id.my.hendisantika.jpapagination.entity.User;
 import id.my.hendisantika.jpapagination.repository.UserRepository;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,7 +13,11 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SpringDataJpaPaginationApplicationTests {
@@ -50,6 +55,30 @@ class SpringDataJpaPaginationApplicationTests {
     @DisplayName("Test MySQL Container Is Running")
     void testMySQLContainerIsRunning() {
         assertThat(mysql.isRunning()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Test Save User")
+    void testSaveUser() {
+        userRepository.saveAll(List.of(
+                new User("Itadori Yuji", "yuji@yopmail.com", "ACTIVE", "TOKYO", "08123456789", "https://randomuser.me/api/portraits/men/85.jpg"),
+                new User("Fushiguro Megumi", "megumi@yopmail.com", "ACTIVE", "TOKYO", "08123456700", "https://randomuser.me/api/portraits/men/85.jpg"),
+                new User("Satoru Gojo", "gojo@yopmail.com", "ACTIVE", "TOKYO", "08123456701", "https://randomuser.me/api/portraits/men/85.jpg"),
+                new User("Suguru Geto", "geto@yopmail.com", "ACTIVE", "TOKYO", "08123456702", "https://randomuser.me/api/portraits/men/85.jpg"),
+                new User("Ryoumen Sukuna", "sukuna@yopmail.com", "INACTIVE", "TOKYO", "08123456703", "https://randomuser.me/api/portraits/men/85.jpg")
+        ));
+
+//        User users = userRepository.findUsersByEmail("yuji@yopmail.com");
+//        List<User> users = userRepository.findUsersByEmail("yuji@yopmail.com");
+        List<User> users = userRepository.findAll();
+
+        assertNotNull(users);
+        assertEquals(5, users.size());
+        assertEquals("Itadori Yuji", users.get(0).getName());
+        assertEquals("Fushiguro Megumi", users.get(1).getName());
+        assertEquals("Satoru Gojo", users.get(2).getName());
+        assertEquals("Suguru Geto", users.get(3).getName());
+        assertEquals("Ryoumen Sukuna", users.get(4).getName());
     }
 
 }
